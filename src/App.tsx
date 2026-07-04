1525fc727884d543a8e0d39d55eaec0c2097c7e4
squaresncubes <prathyusha1113@gmail.com>
2026-07-04 16:21:35 +0530

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Portfolio } from './pages/Portfolio';
 import { Services } from './pages/Services';
@@ -26,7 +26,7 @@ function App() {
   }, []);
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <AnimatePresence mode="wait">
         {loading && <SplashScreen onComplete={() => setLoading(false)} />}
       </AnimatePresence>
@@ -42,7 +42,7 @@ function App() {
           <Route path="*" element={<Home />} />
         </Routes>
       )}
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
